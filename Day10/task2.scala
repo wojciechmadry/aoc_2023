03913bcdd85084012a1400622b0240bb9fbcc910
Wojciech Madry <madrywojciech99@gmail.com>
2023-12-10 20:52:42 +0100

    Day10 Part2 - Idea
    Signed-off-by: Wojciech Madry <madrywojciech99@gmail.com>

--- Day10/task2.scala
+++ Day10/task2.scala
@@ -3,25 +3,83 @@ import scala.io.Source
 object D10T2 {
     class Pair(var x: Int, var y: Int)
 
-    def fill(p: Pair, map : Array[Array[Char]]) : Boolean = {
+    def fill(p: Pair, map : Array[Array[Char]], visited: scala.collection.mutable.ArrayBuffer[Pair]) : Boolean = {
+        if(visited.find(pp => (pp.x == p.x && pp.y == p.y)) != None) {
+            return true
+        }
+        visited += p
         val newPos = Array[Pair](Pair(p.x - 1, p.y), Pair(p.x + 1, p.y), Pair(p.x, p.y - 1), Pair(p.x, p.y + 1))
+        var me = map(p.y)(p.x)
+        if (me == '.') {
+            map(p.y)(p.x) = 'O'
+            me = 'O'
+        }
         for((pos, i) <- newPos.zipWithIndex) {
-            if(pos.x >= 0 && pos.y >= 0 && pos.x < map(0).length && pos.y < map.length
-                && map(pos.y)(pos.x) == '.') {
-                map(pos.y)(pos.x) = 'O'
-                fill(pos, map)
+            if(pos.x >= 0 && pos.y >= 0 && pos.x < map(0).length && pos.y < map.length) {
+                if(map(pos.y)(pos.x) == '.') {
+                    map(pos.y)(pos.x) = 'O'
+                    fill(pos, map, visited)
+                }
+                val c = map(pos.y)(pos.x)
+                val canGoUpDown = (c == '|' || c == 'J' || c == '7' || c == 'F' || c == 'L') && (me == '.' || me == 'O' || me == '|' || me == 'J' || me == '7' || me == 'F' || me == 'L')
+                if(i == 0 && c == '-') {
+                    //fill(pos, map, visited)
+                } else if (i == 1 && c == '-') {
+                    //fill(pos, map, visited)
+                } else if (i == 2 && canGoUpDown) {
+                    //fill(pos, map, visited)
+                } else if (i == 3 && canGoUpDown) {
+                    //fill(pos, map, visited)
+                }
             }
         }
         true
     }
 
     def printMap(map: Array[Array[Char]]) = {
+        println("=======================")
         for(row <- map) {
             for(col <- row) {
-                print(col + " ")
+                var v = col
+               // if(v != '.' && v != 'O') {
+               //     v = 'X'
+               // }
+                print(v + " ")
             }
             println("")
         }
+        println("=======================")
+    }
+
+    def findFill(pipe: Char) : Char = {
+        if(pipe == '|') {
+            return '|'
+        }
+        return 'X'
+    }
+
+    def resize(old: Array[Array[Char]]) : Array[Array[Char]] = {
+        var res = new Array[Array[Char]](old.length * 2)
+        for(i <- 0 until res.length) {
+            res(i) = new Array[Char](old(0).length * 2)
+            for(j <- 0 until res(i).length) {
+                res(i)(j) = '.'
+            }
+        }
+        for(y <- 0 until old.length) {
+            for(x <- 0 until old(y).length) {
+                res(y*2)(x*2) = old(y)(x)
+                val myC = old(y)(x)
+                if(myC != '.') {
+                    val up = Pair(x * 2, y * 2 - 1)
+                    if(up.y >= 0) {
+                        res(up.y)(up.x) = findFill(myC)
+                    }
+                    //val down = Pair(j + 1, i)
+                }
+            }
+        }
+        return res
     }
 
     def calculateDist(filename: String): Int = {
@@ -33,23 +91,25 @@ object D10T2 {
                 map(i)(j) = c
             }
         }
+        var nmap = resize(map)
+        var visited = new scala.collection.mutable.ArrayBuffer[Pair](0)
         for(i <- 0 until map.length) {
             if(map(i)(0) == '.') {
-                fill(Pair(0, i), map)
+                //fill(Pair(0, i), map, visited)
             }
             if(map(i)(map(i).length - 1) == '.') {
-                fill(Pair(map(i).length - 1, i), map)
+                //fill(Pair(map(i).length - 1, i), map, visited)
             }
         }
         for(i <- 0 until map(0).length) {
             if(map(0)(i) == '.') {
-                fill(Pair(i, 0), map)
+                //fill(Pair(i, 0), map, visited)
             }
             if(map(map.length - 1)(i) == '.') {
-                fill(Pair(i, map.length - 1), map)
+                //fill(Pair(i, map.length - 1), map, visited)
             }
         }
-        printMap(map)
+        printMap(nmap)
         var enclosed = 0
         for(row <- map) {
             for(c <- row) {
@@ -62,9 +122,9 @@ object D10T2 {
     }
     def main(args: Array[String]) = {
         assert(calculateDist("example.txt") == 4, "example.txt != 4")
-        assert(calculateDist("example1.txt") == 4, "example1.txt != 4")
-        assert(calculateDist("example2.txt") == 8, "example2.txt != 8")
-        assert(calculateDist("example3.txt") == 10, "example3.txt != 10")
-        //println("Dist = " + calculateDist("input.txt"))
+        //assert(calculateDist("example1.txt") == 4, "example1.txt != 4")
+       // assert(calculateDist("example2.txt") == 8, "example2.txt != 8")
+       // assert(calculateDist("example3.txt") == 10, "example3.txt != 10")
+       // println("Dist = " + calculateDist("input.txt"))
     }
 }
